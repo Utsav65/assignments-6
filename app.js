98058650c294dc65429c2ddbcae090c837fdcb72
Utsav65 <upatel65@myseneca.ca>
2022-12-12 19:21:45 -0500

    Added Port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const app = express()
 
+const port = process.env.port || 3500
 app.set('view-engine','ejs')
 app.get('/',(req,res)=>{
     res.send("Welcome From the First Cyclic Demo App!! ")
@@ -10,6 +11,6 @@ app.get('/',(req,res)=>{
 app.get('/display',(req,res)=>{
     res.render("display.ejs",{author:'Utsav Patel-156587206'})
 })
-app.listen(3500,()=>{
+app.listen(port,()=>{
     console.log("App is listening at port 3500!!!!")
 })
